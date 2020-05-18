efc64a45293c2057d8e3f14e0efe06b23008f99e
pcsoket <pcsoket@naver.com>
2020-05-18 18:04:48 +0900

    add IndexController

---
+++ src/test/java/com/pcsoket/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,13 @@
+package com.pcsoket.book.springboot.web;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IndexControllerTest {
+
+    @Test
+    public void index_불러온다() {
+
+    }
+}
